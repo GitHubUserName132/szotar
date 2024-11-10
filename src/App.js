9b3e3277ea8a947090a70795b0fb5174453b236d
TranRobi <dtranduy@gmail.com>
2024-11-10 16:47:15 +0100

    final dictionary

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import './App.css';
-import words from './words';
+import "./App.css";
+import words from "./words";
+import { useState } from "react";
 
 function App() {
-  
-  return (
-    <>
-    {
-      words.map(data => {
-        return <div id='datas'>
-          <div>
-            <div id='termEn'>{data.term_en}</div>
-            <div id='termHu'>{data.term_hu}</div>
-          </div>
-          <div>
-            <div id='descEn'>{data.description_en}</div>
-            <div id='descEn'>{data.description_hu}</div>
-          </div>
-        </div>
-      })
-    }
-    </>
-  );
+	let [search, setSearch] = useState("");
+	return (
+		<>
+			<form>
+				<input
+					type="search"
+					className="bg-gray-700 block mr-auto ml-auto p-2 w-4/5 mt-4 mb-4"
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+			</form>
+			{words
+				.filter((e) =>
+					search.toLowerCase() === ""
+						? e
+						: e.term_en.includes(search) || e.term_hu.includes(search)
+				)
+				.map((data) => {
+					return (
+						<div id="datas" className="flex ">
+							<div className="rounded border-black border-2 p-4 w-full enSec">
+								<h1 id="termEn">{data.term_en}</h1>
+								<p id="descEn">{data.description_en}</p>
+							</div>
+							<div className="rounded border-black border-2 p-4 w-full huSec">
+								<h1 id="termEn">{data.term_hu}</h1>
+								<p id="descHu">{data.description_hu}</p>
+							</div>
+						</div>
+					);
+				})}
+		</>
+	);
 }
 
 export default App;
